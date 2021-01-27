7890bd0248a6065578fcfb8757e2e5cc248129a2
chipcom <vbaykin@mail.ru>
2021-01-27 08:49:15 +0300

    edit create_dbf.bat

--- create_dbf.bat
+++ create_dbf.bat
@@ -3,3 +3,7 @@ copy *.dbf d:\_mo\_arc\
 copy *.dbt d:\_mo\_arc\
 copy *.txt d:\_mo\_arc\
 copy *.ttt d:\_mo\_arc\
+copy *.dbf d:\_mo\chip\EXE\
+copy *.dbt d:\_mo\chip\EXE\
+copy *.txt d:\_mo\chip\EXE\
+copy *.ttt d:\_mo\chip\EXE\
