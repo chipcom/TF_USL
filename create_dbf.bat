cc026f0401b70fe24618468d36a0b272370b1769
chipcom <vbaykin@mail.ru>
2022-02-04 11:37:14 +0300

    исправлен create_dbf.bat

--- create_dbf.bat
+++ create_dbf.bat
@@ -1,10 +1,14 @@
 rem _mo21usl
 create_dict
+copy d:\_mo\tf_usl\1\_mo2unit.dbf .
 copy *.dbf d:\_mo\_arc\
 copy *.dbt d:\_mo\_arc\
+del d:\_mo\_arc\v001.dbf
 rem copy *.txt d:\_mo\_arc\
 rem copy *.ttt d:\_mo\_arc\
 copy *.dbf d:\_mo\chip\EXE\
 copy *.dbt d:\_mo\chip\EXE\
+rem copy d:\_mo\tf_usl\1\_mo2unit.dbf d:\_mo\chip\EXE\
+del d:\_mo\chip\EXE\v001.dbf
 rem copy *.txt d:\_mo\chip\EXE\
 rem copy *.ttt d:\_mo\chip\EXE\
