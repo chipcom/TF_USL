82b35f66240e1bebd95d69c151484770738eab56
chipcom <vbaykin@mail.ru>
2023-02-03 11:30:16 +0300

    edit

--- create_dbf.bat
+++ create_dbf.bat
@@ -1,5 +1,5 @@
 create_dict -in=d:\_mo\tf_usl\in -out=d:\_mo\tf_usl\out 2>log.txt
-copy d:\_mo\tf_usl\1\_mo3unit.dbf .\out
+rem copy d:\_mo\tf_usl\1\_mo3unit.dbf .\out
 copy d:\_mo\tf_usl\vmp\_mo3vmp_usl.dbf .\out
 copy d:\_mo\tf_usl\vmp\_mo3vmp_usl.dbt .\out
 copy d:\_mo\tf_usl\src_t005\_mo_t005.dbf .\out
